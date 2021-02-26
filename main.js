fb22c6fe144d8580538faad51531a1fe5fd10007
Koran Agan <koranagan@gmail.com>
2021-02-26 18:16:34 -0500

    add transposition feature

--- main.js
+++ main.js
@@ -39,6 +39,7 @@ function playPiano(e) {
    if (document.querySelector('.blink')) return evaluateGuess(e); //user triggers second ('a guess') event after blinkAll() is called and thus calls evaluateGuess(e)
    getPianoSound(e);
    pressPianoKey(e);
+   
    if (!document.querySelector('.piano.et-mode')) return; // if e-trainer mode not active, exit function
    let range = setRange(); 
    playRandomPitch(range); 
@@ -88,39 +89,56 @@ function setRange() {
       if (getComputedStyle(button).color == 'rgb(200, 200, 200)') {
          range = 15;
       } else if (getComputedStyle(button).color == 'rgb(0, 128, 0)') {
-         range = 9;
+         range = 8;//nine notes minus the root
       }
    return range;
 }
 // +++++++++++++++++++++++++++++
 function playRandomPitch(range) {
+   // random index to pick random note
+   let random_index = Math.floor(Math.random() * Math.floor(range));
+   console.log('random index ' + random_index)
+   if (random_index == 0) random_index = 1; //avoid unison
+   console.log('random index after ' + random_index)
    
-   const random_index = Math.floor(Math.random() * Math.floor(range));
+
+   // pool for chromatic scale (all the 15 notes, octave plus two)
    const chromatic_scale = document.querySelectorAll('audio');
-
-   const diat_keys = document.querySelectorAll('.keys.ivory .key');
-   let diatonic_data_keys = Array.from(diat_keys).map(item => item.getAttribute('data-key'))
+   
+   // ++ pool for diatonic scale (9 notes, octave plus maj9th) ++
+   // chosen indexes represent the diatonic (1, 1, 1/2, 1, 1, 1, 1/2) pattern plus extra whole step for maj9th
+   // let maj_scale_index = [0, 2, 4, 5, 7, 9, 11, 12, 14]
    let diatonic_scale = [];
-
-   for (let i = 0; i < chromatic_scale.length; i++) {
-      for (let j = 0; j < diatonic_data_keys.length; j++) {
-         if (chromatic_scale[i].getAttribute('data-key') == diatonic_data_keys[j]) {
-            diatonic_scale.push(chromatic_scale[i]);
-         }
-      }
+   // iterate trough chromatic scale, 
+   for (let i = 1; i < 9; i++) {//start on 2nd index (i=1) to skip the unison
+      diatonic_scale.push(chromatic_scale[i]);
    }
-  
+   // for (let i = 0; i < maj_scale_index.length; i++) {
+   //    let index = maj_scale_index[i];
+   //    diatonic_scale.push(chromatic_scale[index]);
+   // }
+   
+   // console.log(diatonic_scale)
+   // // let data_key = diatonic_scale.map(item => item.attributes[0].value);
+   let src = diatonic_scale.map(item => item.attributes[1].value);
+   console.log(src)
+   // console.log(chromatic_scale)
+   // let i = 0;
+   // while (i < chromatic_scale.length) {
+   //    console.log(chromatic_scale[i])
+   //    i++
+   // }
+   // debugger
    if (range == 15) {
       _TEST_NOTE = chromatic_scale[random_index];
-      
-   } else if (range == 9) {
+   } else if (range == 8) {
       _TEST_NOTE = diatonic_scale[random_index];
-
    }
    setTimeout(function () {
       _TEST_NOTE.play();
       document.querySelector('#test-note').innerHTML = "?"
    }, 800) 
+   console.log(_TEST_NOTE)
 }
 // ++++++++++++++++++
 function blinkAll(on) {
@@ -144,77 +162,23 @@ function blinkAll(on) {
       }, 800)
    }
 }
-//////////////////////////
-function loadNewKeyOf() {
 
-   // 1. initiate new 'newTonality' array where the new tonality will be stored
-   let newTonality = [];
-   // 2. make list and store all the pointers (src attribute values) into an array and iterate trough them
-   let srcPointersList = [
-      {src: 'samples/01E2.mp3', name: 'E' },
-      {src: 'samples/02F2.mp3', name: 'F' },
-      {src: 'samples/03Gb2.mp3', name: 'G&#9837;' },
-      {src: 'samples/04G2.mp3', name: 'G' },
-      {src: 'samples/05Ab2.mp3', name: 'A&#9837;' },
-      {src: 'samples/06A2.mp3', name: 'A' },
-      {src: 'samples/07Bb2.mp3', name: 'B&#9837;' },
-      {src: 'samples/08B2.mp3', name: 'B' },
-      {src: 'samples/09C3.mp3', name: 'C' },
-      {src: 'samples/10Db3.mp3', name: 'D&#9837;' },
-      {src: 'samples/11D3.mp3', name: 'D' },
-      {src: 'samples/12Eb3.mp3', name: 'E&#9837;' },
-      {src: 'samples/13E3.mp3', name: '' },
-      {src: 'samples/14F3.mp3', name: '' },
-      {src: 'samples/15Gb3.mp3', name: '' },
-      {src: 'samples/16G3.mp3', name: '' },
-      {src: 'samples/17Ab3.mp3', name: '' },
-      {src: 'samples/18A3.mp3', name: '' },
-      {src: 'samples/19Bb3.mp3', name: '' },
-      {src: 'samples/20B3.mp3', name: '' },
-      {src: 'samples/21C4.mp3', name: '' },
-      {src: 'samples/22Db4.mp3', name: '' },
-      {src: 'samples/23D4.mp3', name: '' },
-      {src: 'samples/24Eb4.mp3', name: '' },
-      {src: 'samples/25E4.mp3', name: '' },
-      {src: 'samples/26F4.mp3', name: '' },
-   ] 
-   // 3. get index of new tonality - indexOf()
-   let current_keyOf = 'E&#9837;'//temporary hypotethical
-   let startIndex_of_src = srcPointersList.findIndex(item => item.name == current_keyOf)
-   // console.log(test)
-   // 4. isolate attributes (src) and push them into ->
-   let newList = srcPointersList.slice(startIndex_of_src, startIndex_of_src + 15);
-   // 5. newTonality.push() all samples (attribute src='string') from that index to the length of current key (15 notes)
-   newList.map(item => newTonality.push(item.src));
-   console.log(newTonality)
-   
-   // 6. construct nested for loop and set new attributes
-   let samples = document.querySelectorAll('audio');
-   console.log(samples)
-
-   for (let i = 0; i < samples.length; i++) {
-      for (let j = 0; j < newTonality.length; j++) {
-         samples[i].setAttribute('src', `${newTonality[0]}`)
-      }
-      
+// +++++++++++++++++++++++
+function constructScale(chromaticArr, scaleIndexArr) {
+   let scale = [];
+   for (let i = 0; i < scaleIndexArr.length; i++) {
+      let index = scaleIndexArr[i];
+      scale.push(chromaticArr[index]);
    }
-   console.log(samples)
-   
+   return scale;
 }
-/////////////////////////
-
-
 // +++++++++++++++++++++++
 let keyOfIndex = 0;
 let keyOfArr = ['C', 'D&#9837;','D', 'E&#9837;', 'E', 'F', 'G&#9837;', 'G', 'A&#9837;', 'A', 'B&#9837;', 'B'];
 function transposeKeyOf() {
    if (getComputedStyle(document.
       getElementById('diatonic-toggle')).color == 'rgb(200, 200, 200)') return;
-   
-   // load samples
-
-
-   // trainer display corresponding tonalities   
+// trainer display corresponding tonalities   
    keyOfIndex += 1;
    if (keyOfIndex == keyOfArr.length) keyOfIndex = 0;
    let current_keyOf = document.getElementById('key-of').innerHTML = keyOfArr[keyOfIndex];
@@ -224,9 +188,76 @@ function transposeKeyOf() {
       trans_button.style.color = 'rgb(235, 218, 132)';
    } else if (keyOfIndex == 0) {
       trans_button.style.background = 'rgb(239, 239, 239)';
-   }
+   };
 
+// ++LOAD AUDIO SAMPLES FOR NEW TE TONALITY++
+// 1. initiate new 'oneOctaveSrc' array where src's for entire octave vill be stored
+let oneOctaveSrc = [];
+// 2. make list and store all the pointers (src attribute values) into an array and iterate trough them
+let srcPointersList = [
+         {src: 'samples/01E2.mp3', name: 'E' },
+         {src: 'samples/02F2.mp3', name: 'F' },
+         {src: 'samples/03Gb2.mp3', name: 'G&#9837;' },
+         {src: 'samples/04G2.mp3', name: 'G' },
+         {src: 'samples/05Ab2.mp3', name: 'A&#9837;' },
+         {src: 'samples/06A2.mp3', name: 'A' },
+         {src: 'samples/07Bb2.mp3', name: 'B&#9837;' },
+         {src: 'samples/08B2.mp3', name: 'B' },
+         {src: 'samples/09C3.mp3', name: 'C' },
+         {src: 'samples/10Db3.mp3', name: 'D&#9837;' },
+         {src: 'samples/11D3.mp3', name: 'D' },
+         {src: 'samples/12Eb3.mp3', name: 'E&#9837;' },
+         {src: 'samples/13E3.mp3', name: '' },
+         {src: 'samples/14F3.mp3', name: '' },
+         {src: 'samples/15Gb3.mp3', name: '' },
+         {src: 'samples/16G3.mp3', name: '' },
+         {src: 'samples/17Ab3.mp3', name: '' },
+         {src: 'samples/18A3.mp3', name: '' },
+         {src: 'samples/19Bb3.mp3', name: '' },
+         {src: 'samples/20B3.mp3', name: '' },
+         {src: 'samples/21C4.mp3', name: '' },
+         {src: 'samples/22Db4.mp3', name: '' },
+         {src: 'samples/23D4.mp3', name: '' },
+         {src: 'samples/24Eb4.mp3', name: '' },
+         {src: 'samples/25E4.mp3', name: '' },
+         {src: 'samples/26F4.mp3', name: '' },
+] 
+// 3. get index of starting note in new tonality
+let startingNoteIndex = srcPointersList.findIndex(item => item.name == current_keyOf)
+// 4. isolate attributes (src) and push them into ->
+let oneOctaveList = srcPointersList.slice(startingNoteIndex, startingNoteIndex + 15);
+// 5. oneOctaveSrc.push() all samples (attribute src='string') from that index to the length of current key (15 notes)
+oneOctaveList.map(item => oneOctaveSrc.push(item.src));
+// rearrange - move the ebony keys to the back of the newTonality array
+// this step is neccessary because in the '.piano' layout (index.html) '.keys ivory' && '.keys ebony'
+// are in separate div elements
+scale_index = [0, 2, 4, 5, 7, 9, 11, 12, 14]
+let newTonality = [];
+   let ivory_keys = constructScale(oneOctaveSrc, scale_index);
+   ivory_keys.map(item => newTonality.push(item));
+let ebony_keys = [];
+for (let i = 0; i < oneOctaveSrc.length; i++) {
+   if (!ivory_keys.includes(oneOctaveSrc[i])) {
+      ebony_keys.push(oneOctaveSrc[i])
+   }
 }
+   ebony_keys.map(item => newTonality.push(item))
+
+// console.log(newTonality)
+// console.log(ebony_keys)
+// console.log(ivory_keys)
+// // console.log(newTonality)
+// console.log(oneOctaveSrc)
+   
+// 6. construct nested for loop and set new attributes
+let samples = document.querySelectorAll('audio');
+for (let i = 0; i < newTonality.length; i++) {
+samples[i].setAttribute('src', `${newTonality[i]}`)
+};
+// console.log(samples)
+// console.log(newTonality)
+}
+
 // +++++++++++++++++++++++
 function toggleDiatonic() {
    let cKey = document.getElementById('C');
@@ -238,12 +269,11 @@ function toggleDiatonic() {
       button.style.color = 'green';
       key_of.innerHTML = 'C';
       cKey.style.background = 'rgb(235, 218, 132)';
-      
    } else {
       button.style.color = 'rgb(200, 200, 200)';
       key_of.innerHTML = '';
       cKey.style.background = 'rgb(243, 242, 237)';
-   }
+   };
 }
 // +++++++++++++++++++++
 function toggleOnOff() {
@@ -281,13 +311,12 @@ function evaluateGuess(e) {
       document.getElementById('test-note').style.color = 'red';
    } else {
       document.getElementById('test-note').style.color = 'green';
-      
-   }
+   };
    // these values of 'answers' only to be displayed for comparing user's input.
    correct_answer = document.
-      querySelector(`.key[data-key="${test_note}"]`).getAttribute('id');
+   querySelector(`.key[data-key="${test_note}"]`).getAttribute('id');
    wrong_answer = document.
-      querySelector(`.key[data-key="${guess}"]`).getAttribute('id');
+   querySelector(`.key[data-key="${guess}"]`).getAttribute('id');
    
    // RESULT OUTPUT
    let para = document.createElement("P");
